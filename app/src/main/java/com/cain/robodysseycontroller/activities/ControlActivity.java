3a0cd3e96c0ba397a336a2a72b354e46326ba3cb
Sumpatel <sumpatel@gmail.com>
2016-11-24 22:54:48 -0700

    added ip webcam feed to control activity

--- app/src/main/java/com/cain/robodysseycontroller/activities/ControlActivity.java
+++ app/src/main/java/com/cain/robodysseycontroller/activities/ControlActivity.java
@@ -26,6 +26,10 @@ public class ControlActivity extends AppCompatActivity implements
 
     private static final int SERVERPORT = 8888;
     private static final String SERVER_IP = "10.0.2.2";
+    private static final String VIDEOFEED = "http://192.168.1.65:8080/video";
+            //"http://www.ebookfrenzy.com/android_book/movie.mp4";
+            //
+
 
     // Creates activity and turns it into immersive/manual mode
     @Override
@@ -40,12 +44,24 @@ public class ControlActivity extends AppCompatActivity implements
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
                         | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        mDetector = new GestureDetectorCompat(this,this);
+        mDetector = new GestureDetectorCompat(this, this);
         // Set the gesture detector as the double tap
         // listener.
         mDetector.setOnDoubleTapListener(this);
 
         new Thread(new ClientThread()).start();
+
+        // code for video feed here
+        new Thread(new MjpegMaker()).start();
+
+    }
+
+    class MjpegMaker implements Runnable {
+        @Override
+        public void run() {
+            MjpegView vidView = (MjpegView) findViewById(R.id.vidView);
+            vidView.setSource(MjpegInputStream.read(VIDEOFEED));
+        }
     }
 
     class ClientThread implements Runnable {
@@ -59,23 +75,23 @@ public class ControlActivity extends AppCompatActivity implements
                 dataOut = new DataOutputStream(clientSocket.getOutputStream());
             } catch (UnknownHostException e) {
                 e.printStackTrace();
-            } catch (IOException e){
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    public void SendTransmission(String command){
-        try{
+    public void SendTransmission(String command) {
+        try {
             dataOut.writeBytes(command + '\n');
             //dataOut.writeUTF(command + '\n'); // right and up does not work with 'r' and 'u' for whatever reason
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event){
+    public boolean onTouchEvent(MotionEvent event) {
         this.mDetector.onTouchEvent(event);
         // Be sure to call the superclass implementation
         return super.onTouchEvent(event);
@@ -90,20 +106,20 @@ public class ControlActivity extends AppCompatActivity implements
     @Override
     public boolean onFling(MotionEvent event1, MotionEvent event2,
                            float velocityX, float velocityY) {
-        if (tourMode){
+        if (tourMode) {
             Log.d("Sound: ", "in Tour Mode, double tap to engage controls");
-        }else{
+        } else {
             float minDistance = 40;
-            if(event1.getX() - event2.getX() > minDistance && Math.abs(velocityX) > Math.abs(velocityY)){
+            if (event1.getX() - event2.getX() > minDistance && Math.abs(velocityX) > Math.abs(velocityY)) {
                 Log.d("Direction: ", "Right");
                 SendTransmission("right");
-            }else if (event1.getX() - event2.getX() < minDistance && Math.abs(velocityX) > Math.abs(velocityY)){
+            } else if (event1.getX() - event2.getX() < minDistance && Math.abs(velocityX) > Math.abs(velocityY)) {
                 Log.d("Direction: ", "Left");
                 SendTransmission("left");
-            }else if (event1.getY() - event2.getY() > minDistance && Math.abs(velocityX) <= Math.abs(velocityY)) {
+            } else if (event1.getY() - event2.getY() > minDistance && Math.abs(velocityX) <= Math.abs(velocityY)) {
                 Log.d("Direction: ", "Up");
                 SendTransmission("up");
-            }else if (event1.getY() - event2.getY() < minDistance && Math.abs(velocityX) <= Math.abs(velocityY)) {
+            } else if (event1.getY() - event2.getY() < minDistance && Math.abs(velocityX) <= Math.abs(velocityY)) {
                 Log.d("Direction: ", "Down");
                 SendTransmission("down");
             }
@@ -135,10 +151,10 @@ public class ControlActivity extends AppCompatActivity implements
     // see name
     @Override
     public boolean onDoubleTap(MotionEvent event) {
-        if (tourMode){
+        if (tourMode) {
             Log.d("Mode Switched: ", "Manual Mode");
             SendTransmission("manual");
-        }else{
+        } else {
             Log.d("Mode Switched: ", "Tour Mode");
             SendTransmission("tour");
         }
